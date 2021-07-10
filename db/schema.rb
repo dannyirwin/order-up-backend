0777da582f80a429e58662291ff87c7c73d9b617
dannyirwin <dirwin123@gmail.com>
2021-07-09 19:15:53 -0600

    joinGame functionality added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_172921) do
+ActiveRecord::Schema.define(version: 2021_07_09_211723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_172921) do
     t.string "key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "private", default: false
   end
 
   add_foreign_key "game_cards", "cards"
